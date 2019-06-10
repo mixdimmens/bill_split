219f14ff86d4b67661f5e0879b6fac8f6783b8ea
mixdimmens <maximandrediamond@mgail.com>
2019-06-09 21:10:28 -0400

    repr logic fixes

--- bill_split_r1.py
+++ bill_split_r1.py
@@ -2,6 +2,7 @@ import time
 from decimal import Decimal as ds
 
 class Transaction:
+    init_swtch = False
     pay_switch = False
     ledger = []
     paid_off = []
@@ -45,7 +46,6 @@ class Transaction:
                 self.ledger.pop(self.find_debtor_index(debtor))
             else:
                 print('something else is')
-            
             # self.debtors.remove(debtor)
         except ValueError:
             print('some shit is off!')
@@ -54,29 +54,28 @@ class Transaction:
         # try:
         print(debtor)
         debtor_index = 0
-        # for i in range(len(self.ledger)):
-        #     if self.ledger[i][0] == debtor:
-        #         debtor_index = i
-        #         print('check it: {}'.format(self.ledger[i][0]))
         if len(self.ledger) == 0:
             pass
         else:
             self.ledger[debtor_index][1] = self.ledger[self.find_debtor_index(debtor)][1] - ammount
-            # print(debtor_index)
             self.pay_switch = True
         # except:
         #     print('"paid_in" not working out')
     
     def __repr__(self):
-        self.splt()
-        if len(self.ledger) == 0:
+        # self.splt()
+        if self.init_swtch == False:
+            out_str = str([self.transaction_name, self.traaction_date, self.splt()])
+            self.init_swtch = True
+        elif len(self.ledger) == 0:
             out_str = "everyone's paid off!"  
         elif self.pay_switch == True:
             out_str = str([self.transaction_name, self.traaction_date, self.ledger])
-        else:
+        # elif self.init_swtch == False:
             # out_str = str([self.transaction_name, self.traaction_date, self.splt()])
-            out_str = str([self.transaction_name, self.traaction_date, self.ledger])
+            # out_str = str([self.transaction_name, self.traaction_date, self.ledger])
             # out_str = str(self.splt())
+            # self.init_swtch = True
         return out_str
 
 
@@ -87,11 +86,11 @@ print(testy)
 testy.paid_in('b', 2)
 print(testy)
 
-# testy.paid('a')
-# print(testy)
-# testy.paid('b')
-# print(testy)
-# testy.paid('c')
-# print(testy)
+testy.paid('a')
+print(testy)
+testy.paid('b')
+print(testy)
+testy.paid('c')
+print(testy)
 
 # testy.date_marker()
